3fa84bf2f6c319cc4fc7681bb84678aee398c019
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-08-15 18:17:43 -0500

    Fix an issue in Dogememe

--- cheemsbot/cogs/reddit.py
+++ cheemsbot/cogs/reddit.py
@@ -22,6 +22,7 @@ class RedditCommandsCog(commands.Cog, name="Reddit posts and memes"):
                 self.reddit_post.post_author,
                 self.reddit_post.post_author_avatar,
                 self.reddit_post.post_link,
+                "meme"
             ).get_embed_message()
         await ctx.send(embed=embed_message)
 
