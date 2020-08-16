999df3298ded7a593f4572a5803e2f0303e6e9a7
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-08-16 12:42:09 -0500

    Add typing indicator to general Reddit Posts

--- cheemsbot/cogs/reddit.py
+++ cheemsbot/cogs/reddit.py
@@ -6,7 +6,6 @@ from discord.errors import NotFound
 from discord.ext import commands
 from prawcore.exceptions import NotFound
 
-
 class RedditCommandsCog(commands.Cog, name="Reddit posts and memes"):
     def __init__(self, bot):
         self.bot = bot
@@ -69,25 +68,26 @@ class RedditCommandsCog(commands.Cog, name="Reddit posts and memes"):
         if self.current_subreddit is None:
             await ctx.send("Give a subreddit")
             return
-        try:
-            self.reddit_post = conf.get_reddit_post(self.current_subreddit)
-        except NotFound:
-            await ctx.send("Post not found")
-            return
-        if (self.reddit_post.is_nsfw) and (ctx.channel.is_nsfw() is False):
-            await ctx.send("Not in front of the children.")
-            return
-        self.string_test_result = any(
-            element in self.current_subreddit for element in self.forbidden
-        )
-        if self.string_test_result and (ctx.channel.is_nsfw() is False):
-            await ctx.send(
-                "Subreddit contains a forbidden word. This detection will be improved eventually."  # noqa: E501
+        async with ctx.typing():
+            try:
+                self.reddit_post = conf.get_reddit_post(self.current_subreddit)
+            except NotFound:
+                await ctx.send("Post not found")
+                return
+            if (self.reddit_post.is_nsfw) and (ctx.channel.is_nsfw() is False):
+                await ctx.send("Not in front of the children.")
+                return  
+            self.string_test_result = any(
+                element in self.current_subreddit for element in self.forbidden
             )
-            return
-        embed_message = embeds.RedditEmbedMessage(
-            self.reddit_post, "post",
-        ).get_embed_message()
+            if self.string_test_result and (ctx.channel.is_nsfw() is False):
+                await ctx.send(
+                    "Subreddit contains a forbidden word. This detection will be improved eventually."  # noqa: E501
+                )
+                return
+            embed_message = embeds.RedditEmbedMessage(
+                self.reddit_post, "post",
+            ).get_embed_message()
         await ctx.send(embed=embed_message)
 
 
