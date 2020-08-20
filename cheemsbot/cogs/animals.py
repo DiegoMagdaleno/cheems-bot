27f8d3e28eeb192a68df2773e40f3b138690fae8
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-08-20 13:28:18 -0500

    Fix an issue on Animals.py session init

--- cheemsbot/cogs/animals.py
+++ cheemsbot/cogs/animals.py
@@ -3,28 +3,29 @@ from cheemsbot.helpers import animals
 from discord.ext import commands
 
 
-class AnimalCommandCog(commands.cog, name="Animals"):
+class AnimalCommandCog(commands.Cog, name="Animals"):
     """Set of actions to get some flufy animals from the internet into Discord chat"""
 
     def __init__(self, bot ) -> None:
         self.bot = bot
+        self.animal_session = animals.Animals()
 
     @commands.command(name="shiba")
     async def shiba(self, ctx):
         """Description: Gets an image from Shibas.online api and posts it in chat\nArguments:`none`"""
-        self.our_shiba = animals.Animals.shiba()
+        self.our_shiba = self.animal_session.shiba()
         await ctx.send(self.our_shiba)
     
     @commands.command(name="cat")
     async def cat(self, ctx):
         """Description: Gets a cat image and posts it in chat\nArguments:`none`"""
-        self.our_cat = animals.Animals.cat()
+        self.our_cat = self.animal_session.cat()
         await ctx.send(self.our_cat)
     
     @commands.command(name="dog")
     async def dog(self, ctx):
         """Description: Gets a dog image and posts it in chat\nArguments:`none`"""
-        self.our_dog = animals.Animals.dog()
+        self.our_dog = self.animal_session.dog()
         await ctx.send(self.our_dog)
 
 def setup(bot):
